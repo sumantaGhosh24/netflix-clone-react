7036cd425d9236f0bf721279058c62c7ade713a2
sumanta ghosh <sumantaghosh5333@gmail.com>
2022-10-05 10:49:49 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./App.css";
+import Nav from "./Nav";
+import Banner from "./Banner";
+import Row from "./Row";
+import request from "./request";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={request.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
